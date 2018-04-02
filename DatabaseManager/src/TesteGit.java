71195d0f9517aa083fc3df94d8b252f967913e7f
Andre <Andre@10.12.62.38>
2018-04-02 14:09:03 -0300

    Aham

--- DatabaseManager/src/TesteGit.java
+++ DatabaseManager/src/TesteGit.java
@@ -4,7 +4,7 @@ public class TesteGit {
 	public static void main(String[] args) {
 		System.out.println("Vamos ver se agora essa merda funciona!");
 		System.out.println("Agora funcionou essa merda!!!");
-
+		System.out.println("Ae caraio!!");
 	}
 
 }
