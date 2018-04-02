4c8a05daa321069dc6efa0d49a94448a91415a2c
André Raubach <37275719+Raubach@users.noreply.github.com>
2018-04-02 14:08:10 -0300

    Alterado no GitHub
    Esse caralho!

--- DatabaseManager/src/TesteGit.java
+++ DatabaseManager/src/TesteGit.java
@@ -3,6 +3,7 @@ public class TesteGit {
 
 	public static void main(String[] args) {
 		System.out.println("Vamos ver se agora essa merda funciona!");
+		System.out.println("Agora funcionou essa merda!!!");
 
 	}
 
